ea08ad873180d0aa7930d4dc6728dc1f326833b9
Daniel Couturier <dancout@umich.edu>
2023-07-23 13:14:36 -0400

    Added mutation rate and made Gene abstract

--- lib/models/gene.dart
+++ lib/models/gene.dart
@@ -1,11 +1,29 @@
+import 'dart:math';
+
 import 'package:equatable/equatable.dart';
 
-class Gene<T> extends Equatable {
-  const Gene({
-    required this.value,
-  });
+abstract class Gene<T> extends Equatable {
+  Gene({
+    double mutationRate = 0,
+    required T value,
+  }) {
+    assert(
+      mutationRate >= 0.toDouble() && mutationRate <= 1.toDouble(),
+      'mutationRate must be between 0 and 1, inclusively',
+    );
 
-  final T value;
+    final randomValue = Random().nextDouble();
+
+    if (mutationRate > randomValue) {
+      this.value = mutate();
+    } else {
+      this.value = value;
+    }
+  }
+
+  late final T value;
+
+  T mutate();
 
   @override
   List<Object?> get props => [
