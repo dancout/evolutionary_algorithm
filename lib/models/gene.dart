14c2931821bd1710afb9fd5dc78b12c3edfda1ae
Daniel Couturier <dancout@umich.edu>
2023-07-23 13:46:13 -0400

    Added tests and documentation for Gene

--- lib/models/gene.dart
+++ lib/models/gene.dart
@@ -1,28 +1,40 @@
 import 'dart:math';
 
 import 'package:equatable/equatable.dart';
+import 'package:flutter/foundation.dart';
 
 abstract class Gene<T> extends Equatable {
   Gene({
-    double mutationRate = 0,
+    /// The intended encoded value for this Gene.
     required T value,
+
+    /// A value between 0 and 1, inclusively, that represents if this gene
+    /// should mutate its value. A value of 0 will never mutate and a value of 1
+    /// will always mutate.
+    double mutationRate = 0,
+
+    /// This value will be used to check against the mutationRate to determine
+    /// if the gene should be mutated.
+    @visibleForTesting double? randomValue,
   }) {
     assert(
       mutationRate >= 0.toDouble() && mutationRate <= 1.toDouble(),
       'mutationRate must be between 0 and 1, inclusively',
     );
 
-    final randomValue = Random().nextDouble();
+    final randValue = randomValue ?? Random().nextDouble();
 
-    if (mutationRate > randomValue) {
+    if (mutationRate > randValue) {
       this.value = mutate();
     } else {
       this.value = value;
     }
   }
 
+  /// The encoded value for this gene.
   late final T value;
 
+  /// Returns a mutated encoded value of type <T>.
   T mutate();
 
   @override
---
+++ test/models/gene_test.dart
@@ -0,0 +1,79 @@
+import 'package:evolutionary_algorithm/models/gene.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+const mutatedValue = 100;
+
+void main() {
+  const value = 1;
+
+  group('mutationRate', () {
+    test('less than zero will throw assertion', () async {
+      const mutationRate = -1.0;
+
+      expect(
+        () => FakeGene(value: value, mutationRate: mutationRate),
+        throwsAssertionError,
+      );
+    });
+
+    test('more than one will throw assertion', () async {
+      const mutationRate = 1.1;
+
+      expect(
+        () => FakeGene(value: value, mutationRate: mutationRate),
+        throwsAssertionError,
+      );
+    });
+
+    test('within 0 and 1, inclusively, will complete without error', () async {
+      double mutationRate = 0.0;
+
+      while (mutationRate <= 1.0) {
+        FakeGene(value: value, mutationRate: mutationRate); // Expect no errors
+        mutationRate += 0.1;
+      }
+    });
+  });
+
+  group('mutate', () {
+    test('is called when mutationRate is higher than internal random number',
+        () async {
+      const randomValue = 0.1;
+      const mutationRate = 0.2;
+      final actualValue = FakeGene(
+        value: value,
+        mutationRate: mutationRate,
+        randomValue: randomValue,
+      ).value;
+
+      expect(actualValue, mutatedValue);
+    });
+
+    test(
+        'is not called when mutationRate is higher than internal random number',
+        () async {
+      const randomValue = 0.2;
+      const mutationRate = 0.1;
+      final actualValue = FakeGene(
+        value: value,
+        mutationRate: mutationRate,
+        randomValue: randomValue,
+      ).value;
+
+      expect(actualValue, value);
+    });
+  });
+}
+
+class FakeGene extends Gene<int> {
+  FakeGene({
+    required super.value,
+    super.mutationRate,
+    super.randomValue,
+  });
+
+  @override
+  int mutate() {
+    return mutatedValue;
+  }
+}
