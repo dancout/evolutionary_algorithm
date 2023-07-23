823d103794b519eff9b5eeeff335a85344257ca5
Daniel Couturier <dancout@umich.edu>
2023-07-23 16:38:11 -0400

    Created GeneService to support logic

--- lib/models/gene.dart
+++ lib/models/gene.dart
@@ -1,42 +1,13 @@
-import 'dart:math';
-
 import 'package:equatable/equatable.dart';
-import 'package:flutter/foundation.dart';
 
-abstract class Gene<T> extends Equatable {
-  Gene({
+class Gene<T> extends Equatable {
+  const Gene({
     /// The intended encoded value for this Gene.
-    required T value,
-
-    /// A value between 0 and 1, inclusively, that represents if this gene
-    /// should mutate its value. A value of 0 will never mutate and a value of 1
-    /// will always mutate.
-    double mutationRate = 0,
-
-    /// This value will be used to check against the mutationRate to determine
-    /// if the gene should be mutated.
-    @visibleForTesting double? randomValue,
-  }) {
-    assert(
-      mutationRate >= 0.toDouble() && mutationRate <= 1.toDouble(),
-      'mutationRate must be between 0 and 1, inclusively',
-    );
-
-    final randValue = randomValue ?? Random().nextDouble();
-
-    if (mutationRate > randValue) {
-      this.value = this.mutate(value: value);
-    } else {
-      this.value = value;
-    }
-  }
+    required this.value,
+  });
 
   /// The encoded value for this gene.
-  late final T value;
-
-  /// Returns a mutated encoded value of type <T>.
-  @visibleForTesting
-  T mutate({T? value});
+  final T value;
 
   @override
   List<Object?> get props => [
--- test/services/gene_service_test.dart
+++ test/services/gene_service_test.dart
@@ -1,17 +1,18 @@
 import 'package:evolutionary_algorithm/models/gene.dart';
+import 'package:evolutionary_algorithm/services/gene_service.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-const mutatedValue = 100;
+const mutatedValue = 2;
 
 void main() {
   const value = 1;
-
+  const gene = Gene(value: value);
   group('mutationRate', () {
     test('less than zero will throw assertion', () async {
       const mutationRate = -1.0;
 
       expect(
-        () => FakeGene(value: value, mutationRate: mutationRate),
+        () => FakeGeneService(mutationRate: mutationRate),
         throwsAssertionError,
       );
     });
@@ -20,7 +21,7 @@ void main() {
       const mutationRate = 1.1;
 
       expect(
-        () => FakeGene(value: value, mutationRate: mutationRate),
+        () => FakeGeneService(mutationRate: mutationRate),
         throwsAssertionError,
       );
     });
@@ -29,22 +30,26 @@ void main() {
       double mutationRate = 0.0;
 
       while (mutationRate <= 1.0) {
-        FakeGene(value: value, mutationRate: mutationRate); // Expect no errors
+        () => FakeGeneService(mutationRate: mutationRate); // Expect no errors
         mutationRate += 0.1;
       }
     });
   });
 
-  group('mutate', () {
+  group('mutateGene', () {
     test('is called when mutationRate is higher than internal random number',
         () async {
       const randomValue = 0.1;
       const mutationRate = 0.2;
-      final actualValue = FakeGene(
-        value: value,
-        mutationRate: mutationRate,
-        randomValue: randomValue,
-      ).value;
+
+      final testObject = FakeGeneService(mutationRate: mutationRate);
+
+      final actualValue = testObject
+          .mutateGene(
+            gene: gene,
+            randomValue: randomValue,
+          )
+          .value;
 
       expect(actualValue, mutatedValue);
     });
@@ -54,26 +59,32 @@ void main() {
         () async {
       const randomValue = 0.2;
       const mutationRate = 0.1;
-      final actualValue = FakeGene(
-        value: value,
-        mutationRate: mutationRate,
-        randomValue: randomValue,
-      ).value;
+      final testObject = FakeGeneService(mutationRate: mutationRate);
+
+      final actualValue = testObject
+          .mutateGene(
+            gene: gene,
+            randomValue: randomValue,
+          )
+          .value;
 
       expect(actualValue, value);
     });
   });
 }
 
-class FakeGene extends Gene<int> {
-  FakeGene({
-    required super.value,
-    super.mutationRate,
-    super.randomValue,
+class FakeGeneService extends GeneService {
+  FakeGeneService({
+    required super.mutationRate,
   });
 
   @override
-  int mutate({int? value}) {
+  mutateValue({value}) {
     return mutatedValue;
+  }
+
+  @override
+  Gene randomGene() {
+    throw UnimplementedError();
   }
 }
