81f2ed65d24415d9cb7c8eeec425732df65de2a3
Daniel Couturier <dancout@umich.edu>
2023-07-23 15:48:22 -0400

    Create dna.dart

---
+++ lib/models/dna.dart
@@ -0,0 +1,15 @@
+import 'package:equatable/equatable.dart';
+import 'package:evolutionary_algorithm/models/gene.dart';
+
+abstract class DNA extends Equatable {
+  const DNA({
+    required this.genes,
+  });
+
+  final List<Gene> genes;
+
+  @override
+  List<Object?> get props => [
+        genes,
+      ];
+}
