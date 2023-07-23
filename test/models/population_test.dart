4d8d7034fa773fb9c6559bf83c1573b33641432f
Daniel Couturier <dancout@umich.edu>
2023-07-23 18:13:35 -0400

    Created population.dart

---
+++ test/models/population_test.dart
@@ -0,0 +1,33 @@
+import 'package:evolutionary_algorithm/models/entity.dart';
+import 'package:evolutionary_algorithm/models/population.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import '../mocks.dart';
+
+void main() {
+  group('sortedEntities', () {
+    test('should return entities in sorted order based on fitnessScore',
+        () async {
+      const lowScore = 1.0;
+      const mediumScore = 5.0;
+      const highScore = 10.0;
+
+      final lowScoreEntity = Entity(dna: MockDNA(), fitnessScore: lowScore);
+      final mediumScoreEntity =
+          Entity(dna: MockDNA(), fitnessScore: mediumScore);
+      final highScoreEntity = Entity(dna: MockDNA(), fitnessScore: highScore);
+
+      final expected = [lowScoreEntity, mediumScoreEntity, highScoreEntity];
+
+      final actual = Population(
+        entities: [
+          mediumScoreEntity,
+          highScoreEntity,
+          lowScoreEntity,
+        ],
+      ).sortedEntities;
+
+      expect(actual, expected);
+    });
+  });
+}
