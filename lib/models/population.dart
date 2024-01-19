c6f826ed79b9903c519f1e19bb062d35e337030a
Daniel Couturier <dancout@umich.edu>
2024-01-19 06:37:22 -0500

    Rolled back sorting method on Population and left TODOs

--- lib/models/population.dart
+++ lib/models/population.dart
@@ -3,27 +3,19 @@ part of 'package:genetic_evolution/genetic_evolution.dart';
 /// Represents a collection of [Entity] objects.
 class Population<T> extends Equatable {
   /// Represents a collection of [Entity] objects.
-  Population({
+  const Population({
     required this.entities,
-    int Function(Entity<T> a, Entity<T> b)? sortingMethod,
-  }) {
-    fallbackSortMethod(Entity<T> a, Entity<T> b) =>
-        b.fitnessScore.compareTo(a.fitnessScore);
-
-    this.sortingMethod = sortingMethod ?? fallbackSortMethod;
-  }
+  });
 
   /// Represents the entities present within this population.
   final List<Entity<T>> entities;
 
-  /// Represents the method used to sort [Population.entities].
-  late final int Function(Entity<T> a, Entity<T> b) sortingMethod;
-
   /// Returns a list of entities in this population sorted in order from highest
   /// fitnessScore to lowest.
   List<Entity<T>> get sortedEntities => List.from(entities)
+    // TODO: Try adding sorting method?
     ..sort(
-      sortingMethod,
+      (a, b) => b.fitnessScore.compareTo(a.fitnessScore),
     );
 
   /// Returns the top scoring entity from this population.
@@ -40,6 +32,74 @@ class Population<T> extends Equatable {
   @override
   List<Object?> get props => [
         entities,
-        sortingMethod,
       ];
+// TODO: Remove all this commented out code
 }
+
+// part of 'package:genetic_evolution/genetic_evolution.dart';
+
+// /// Represents a collection of [Entity] objects.
+// class Population<T> extends Equatable {
+//   /// Represents a collection of [Entity] objects.
+//   Population({
+//     required this.entities,
+//     int Function(Entity<T> a, Entity<T> b)? sortingMethod,
+//   }) {
+//     fallbackSortMethod(Entity<T> a, Entity<T> b) =>
+//         b.fitnessScore.compareTo(a.fitnessScore);
+
+//     this.sortingMethod = sortingMethod ?? fallbackSortMethod;
+//   }
+
+//   /// Represents the entities present within this population.
+//   final List<Entity<T>> entities;
+
+//   /// Represents the method used to sort [Population.entities].
+//   late final int Function(Entity<T> a, Entity<T> b) sortingMethod;
+
+//   /// Returns a list of entities in this population sorted in order from highest
+//   /// fitnessScore to lowest.
+//   List<Entity<T>> get sortedEntities => List.from(entities)
+//     ..sort(
+//       sortingMethod,
+//     );
+
+//   /// Returns the top scoring entity from this population.
+//   Entity<T> get topScoringEntity {
+//     return entities.fold(
+//       entities.first,
+//       (previousValue, element) =>
+//           element.fitnessScore > previousValue.fitnessScore
+//               ? element
+//               : previousValue,
+//     );
+//   }
+
+//   @override
+//   List<Object?> get props => [
+//         entities,
+//         sortingMethod,
+//       ];
+// }
+
+
+
+// extension PopulationExtension<T> on Population<T> {
+//   // /// Returns a list of entities in this population sorted in order from highest
+//   // /// fitnessScore to lowest.
+//   // List<Entity<T>> get sortedEntities => List.from(entities)
+//   //   ..sort(
+//   //     (a, b) => b.fitnessScore.compareTo(a.fitnessScore),
+//   //   );
+
+//   /// Returns the top scoring entity from this population.
+//   // Entity<T> get topScoringEntity {
+//   //   return entities.fold(
+//   //     entities.first,
+//   //     (previousValue, element) =>
+//   //         element.fitnessScore > previousValue.fitnessScore
+//   //             ? element
+//   //             : previousValue,
+//   //   );
+//   // }
+// }
